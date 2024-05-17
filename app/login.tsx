94630248bdcd08f2bce9e90a94d099ae21419d8a
Amar1023 <amarcp2@illinois.edu>
2024-05-17 13:47:15 -0500

    Updated styling for login

--- app/login.tsx
+++ app/login.tsx
@@ -3,7 +3,11 @@ import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { Input, Button, Text, View } from "tamagui";
 import { LogIn } from "@tamagui/lucide-icons";
-import { VarelaRound_400Regular } from "@expo-google-fonts/varela-round";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  VarelaRound_400Regular,
+} from "@expo-google-fonts/varela-round";
 
 // import { ThemedText } from '@/components/ThemedText';
 // import { ThemedView } from '@/components/ThemedView';
@@ -17,33 +21,40 @@ export default function LoginScreen() {
     // console.log('Password:', password);
   };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>
-        Create a Username and Password for your REMedy account
-      </Text>
-      <Text style={styles.label}>Username</Text>
-      <Input
-        style={styles.input}
-        onChangeText={setUsername}
-        value={username}
-        placeholder="Enter your username"
-      />
-      <Text style={styles.label}>Password</Text>
-      <Input
-        style={styles.input}
-        onChangeText={setPassword}
-        value={password}
-        secureTextEntry={true}
-        placeholder="Enter your password"
-      />
-      <Link href="/terms" asChild>
-        <Button iconAfter={LogIn} onPress={handleLogin} style={styles.button}>
-          Log In
-        </Button>
-      </Link>
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    VarelaRound_400Regular,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          Create a Username and Password for your REMedy account
+        </Text>
+        <Text style={styles.label}>Username</Text>
+        <Input
+          style={styles.input}
+          onChangeText={setUsername}
+          value={username}
+          placeholder="Enter your username"
+        />
+        <Text style={styles.label}>Password</Text>
+        <Input
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry={true}
+          placeholder="Enter your password"
+        />
+        <Link href="/terms" asChild>
+          <Button iconAfter={LogIn} onPress={handleLogin} style={styles.button}>
+            Log In
+          </Button>
+        </Link>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
