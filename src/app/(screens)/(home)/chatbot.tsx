66c246d3979f4cddde02ddc20235a21e7d5232cd
arnavg34 <arnavg3410@gmail.com>
2024-05-31 19:49:37 -0500

    fixed .env

--- src/app/(screens)/(home)/chatbot.tsx
+++ src/app/(screens)/(home)/chatbot.tsx
@@ -1,6 +1,7 @@
 import Background from "@/src/components/background";
 import { View } from "tamagui";
 
+const apiUrl = process.env.EXPO_PUBLIC_OPENAI_KEY ;
 export default function LoginScreen() {
   return (
     <Background>
